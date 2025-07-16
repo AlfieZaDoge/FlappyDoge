83a4cae5fd9323a51d5345f228e8515216957a37
AlfieZaDoge <yepperyeppersone@gmail.com>
2025-07-16 18:08:17 +0100

    Update utils.py

--- classes/utils.py
+++ classes/utils.py
@@ -1,8 +1,8 @@
 import pygame
 from .settings import W, H, GW, GH
 
-def draw_grass(surf, grass_img, scroll):
+def draw_grass(surf, grass_img, offset):
     y = H - GH
-    pygame.draw.rect(surf, (70, 200, 100), (0, y, W, GH))
     for i in range(W // GW + 2):
-        surf.blit(grass_img, (i * GW - (scroll % GW), y))
+        x = (i * GW) - (offset % GW)
+        surf.blit(grass_img, (x, y))
